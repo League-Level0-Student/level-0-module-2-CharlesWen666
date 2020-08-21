ad37f5b0c5390fd11055f80467887cbac4c20308
Charles <Charles@appledeair.lan>
2020-08-21 16:59:20 -0700

    what a nice unit

--- src/_01_random/_5_magic_8_ball/Magic8Ball.java
+++ src/_01_random/_5_magic_8_ball/Magic8Ball.java
@@ -9,6 +9,8 @@ import java.util.Random;
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below
+public static void main(String[] args) {
+	
 
 	// 2. Get the user to enter a question for the 8 ball to answer
 	
@@ -33,5 +35,5 @@ public class Magic8Ball {
 	// 7. If the random number is 3
 
 	// -- write your own answer
-
+}
 }
