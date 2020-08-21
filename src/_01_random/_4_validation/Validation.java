019486a26377df1a681f3cc4bd393de22d9a6d98
Charles <Charles@appledeair.lan>
2020-08-21 16:58:43 -0700

    what a nice unit

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -9,15 +9,33 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
-		
+		for(int i = 0; i<5; i++) {	
 		Random randomMaker = new Random();
 		
-		int randomNumber = randomMaker.nextInt(5);
+		int no = randomMaker.nextInt(5)+1;
+		System.out.println(no);
 		
-		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		
+		if(no==5) {
+			System.out.println("!");
+		}
+		if(no==4) {
+			System.out.println("srazy");
+		}
+		if(no==3) {
+	System.out.println("so");
+}
+if(no==2) {
+	System.out.println("are");
+}
+if(no==1) {
+	System.out.println("You");
+	System.out.println(no);
+	
+}
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
